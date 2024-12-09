661083c91e1b5b8c37cc5dbcf74961edba81c0ab
Eskinder <Eskinderytbarek323@gmail.com>
2024-12-09 19:33:59 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import MyComponent from "./myComponent.jsx";
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    console.log(value);
+  };
+
   return (
-    <MyComponent/>
-  )
-}
+    <div>
+      <label htmlFor="inputField">Type something: </label>
+      <input
+        type="text"
+        id="inputField"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter text here"
+      />
+    </div>
+  );
+};
 
 export default App;
