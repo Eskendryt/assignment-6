d622df8dc8fd8e5bf5f1d0e19d4646efbacc2d49
Eskinder <Eskinderytbarek323@gmail.com>
2024-12-10 12:42:11 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
-  const [inputValue, setInputValue] = useState('');
 
-  const handleInputChange = (event) => {
-    const value = event.target.value;
-    setInputValue(value);
+  const [userList, setUserList] = useState([
+    {
+      id: 1,
+      name: "Eskndr",
+      age: 10,
+    },
+    {
+      id: 2,
+      name: "Nebi",
+      age: 15,
+    },
+    {
+      id: 3,
+      name: "Thomas",
+      age: 20,
+    },
+  ]);
 
-    console.log(value);
+
+  const handleEdit = (id) => {
+    const updateList = userList.map((item) => {
+      if (id === item.id) {
+        const editedName = prompt("EDit name", item.name);
+        const editedAge = prompt("Edit Age", item.age);
+
+        return {
+          ...item, name: editedName !== null ? editedName : item.name,
+          age: editedAge !== null ? editedAge : item.age,
+        };
+      }
+      return item;
+    });
+    setUserList(updateList);
   };
 
   return (
     <div>
-      <span>Type something: </span>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter text here"
-      />
+      {userList.map((item) => (
+        <div>
+          <p>Name: {item.name}</p>
+          <p>Age: {item.age}</p>
+          <button onClick={() => handleEdit(item.id)}>Edit</button>
+        </div>
+      ))}
     </div>
-  );
-};
+  )
+
+}
 
 export default App;
